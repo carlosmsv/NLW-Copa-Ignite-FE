1696af38b48fa54a4784225458275810f1541212
Carlos <carlos.msv@hotmail.com>
2022-11-16 23:08:12 -0300

    index updated; tailwind; _document

---
+++ src/pages/index.tsx
@@ -0,0 +1,71 @@
+// interface HomeProps {
+//   count: number;
+// }
+
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Image src={logoImg} alt="NLW Copa" />
+
+        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong>
+            <span>+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form>
+          <input type="text" required placeholder='Qual o nome do seu bolão?' />
+          <button type="submit">Criar meu bolão</button>
+        </form>
+
+        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
+
+      </main>
+
+      <Image 
+        src={appPreviewImg} 
+        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa" 
+        quality={100} 
+      />
+    </div>
+  )
+}
+
+// export const getServerSideProps = async() => {
+//   const response = await fetch('http://localhost:3333/pools/count')
+//   const data = await response.json()
+//   console.log(data)
+
+//   return { 
+//     props: {
+//       count: data.count,
+//     }
+//   }
+// }
